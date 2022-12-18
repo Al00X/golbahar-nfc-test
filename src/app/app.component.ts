adf6934414a39a4457181e60940636a4aaa24ef5
Ali X <al00x@outlook.com>
2022-12-18 13:46:43 +0330

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,6 @@ export class AppComponent {
   public sqliteLoaded = false;
   constructor(private platform: Platform, private sqlite: SQLiteService) {
     this.initializeApp();
-    // this.sqliteLoaded = true;
   }
 
   initializeApp() {
